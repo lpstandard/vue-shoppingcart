823b491da76bc0fdf26cbcf68147e2b33bbd8002
Tony Kung <lps.tony@gmail.com>
2020-03-18 17:54:23 +0800

    working on signin page

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  VUE_APP_APIPATH: '"https://vue-course-api.hexschool.io/"',
+  VUE_APP_CUSTOMPATH: '"lpstony"'
 })
--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    
     <router-view/>
+
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+  created() {
+    const api = `${process.env.VUE_APP_APIPATH}/${
+      process.env.VUE_APP_CUSTOMPATH
+    }/products`;
+    console.log(
+      "PATH",
+      process.env.VUE_APP_APIPATH,
+      process.env.VUE_APP_CUSTOMPATH
+    );
+    this.$http.get(api).then(response => {
+      console.log("API", response.data);
+    });
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "./assets/all";
 </style>
